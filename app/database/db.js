4d52836d01cfe01b29d8f893d4aa3cd2e726999e
Jonathan Zanella <jonathanrafaelz@gmail.com>
2017-04-16 18:25:58 -0300

    Correction for mongo db log

--- app/database/db.js
+++ app/database/db.js
@@ -1,9 +1,10 @@
 var mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, function (err, res) {
+var uristring = process.env.MONGODB_URI;
+mongoose.connect(uristring, function (err, res) {
   if (err) {
     console.log ('ERROR connecting to: ' + uristring + '. ' + err);
   } else {
     console.log ('Succeeded connected to: ' + uristring);
   }
-}));
+});
