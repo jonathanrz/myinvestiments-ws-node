ceac74b5b72ab6f0ea840c876ad74413bc4035c0
Jonathan Zanella <jonathanrafaelz@gmail.com>
2017-04-16 18:24:15 -0300

    Adding logs for errors connecting to mongodb

--- app/database/db.js
+++ app/database/db.js
@@ -1,3 +1,9 @@
 var mongoose = require('mongoose');
 
-mongoose.connect(process.env["MONGODB_URI"]);
+mongoose.connect(process.env.MONGODB_URI, function (err, res) {
+  if (err) {
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uristring);
+  }
+}));
