56ce3b6d7611a83a2cb5815d37d0b22b623d5eaf
Jonathan Zanella <jonathanrafaelz@gmail.com>
2017-04-29 15:50:24 -0300

    Add yield method to income

--- app/models/income.js
+++ app/models/income.js
@@ -1,7 +1,7 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
-var IncomeSchema   = new Schema({
+var incomeSchema = new Schema({
     investment: String,
     date: Date,
     quantity: Number,
@@ -9,4 +9,11 @@ var IncomeSchema   = new Schema({
     bought: Number
 });
 
-module.exports = mongoose.model('Income', IncomeSchema);
+incomeSchema.methods.yield = function(originalValue) {
+  result = value - lastValue;
+  if(bought)
+    result -= bought;
+  return result;
+};
+
+module.exports = mongoose.model('Income', incomeSchema);
