1a30fff19cf7c1a6a9c6c239264f3ca8b645ec75
jonathanrz <jonathanrafaelz@gmail.com>
2017-05-10 22:14:20 -0300

    Removing all investiments query for summary report (#18)

--- app/reports/summary.js
+++ app/reports/summary.js
@@ -18,32 +18,24 @@ function generateDiffAndRenderData(report, res) {
 }
 
 exports.generate_report = function(res) {
-  Investment.find(function(err, investments) {
-    if (err)
+  report = {}
+  Income.find().sort('date').exec(function(err, incomes) {
+    if (err) {
       res.send(err);
-
-    report = {}
-    investments.forEach(function(investment, index) {
-      Income.find({investment: investment.id}).sort('date').exec(function(err, incomes) {
-          if (err) {
-            res.send(err);
-            return;
-          }
-          incomes.forEach(function(income) {
-            currentMonth = moment(income.date).format("MM/YYYY");
-            monthData = report[currentMonth];
-            value = 0;
-            if(monthData) {
-              value = monthData["value"];
-            } else {
-              monthData = {};
-            }
-            monthData["value"] = value + income.value;
-            report[currentMonth] = monthData;
-          });
-          if(index == (investments.length - 1))
-            generateDiffAndRenderData(report, res);
-        });
+      return;
+    }
+    incomes.forEach(function(income) {
+      currentMonth = moment(income.date).format("MM/YYYY");
+      monthData = report[currentMonth];
+      value = 0;
+      if(monthData) {
+        value = monthData["value"];
+      } else {
+        monthData = {};
+      }
+      monthData["value"] = value + income.value;
+      report[currentMonth] = monthData;
     });
+    generateDiffAndRenderData(report, res);
   });
 }
