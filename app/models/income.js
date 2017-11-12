c5bbd3a2ab801b55d16b0d938c399d3b54cf8ff1
Jonathan Zanella <jonathanrafaelz@gmail.com>
2017-11-12 20:26:06 -0200

    Add gross, ir and tax to income

--- app/models/income.js
+++ app/models/income.js
@@ -1,25 +1,41 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
 
 var incomeSchema = new Schema({
-    investment: String,
-    date: Date,
-    quantity: Number,
-    value: Number,
-    bought: Number
+  investment: String,
+  date: Date,
+  quantity: Number,
+  value: Number,
+  bought: Number,
+  gross: Number,
+  ir: Number,
+  tax: Number
 });
 
 incomeSchema.methods.yield = function(originalValue) {
   result = this.value - originalValue;
-  if(this.bought)
-    return result - this.bought;
+  if (this.bought) return result - this.bought;
   return result;
 };
 
 incomeSchema.methods.boughtValue = function() {
-  if(this.bought)
-    return this.bought;
+  if (this.bought) return this.bought;
   return 0;
 };
 
-module.exports = mongoose.model('Income', incomeSchema);
+incomeSchema.methods.grossValue = function() {
+  if (this.gross) return this.gross;
+  return 0;
+};
+
+incomeSchema.methods.taxValue = function() {
+  if (this.tax) return this.tax;
+  return 0;
+};
+
+incomeSchema.methods.irValue = function() {
+  if (this.ir) return this.ir;
+  return 0;
+};
+
+module.exports = mongoose.model("Income", incomeSchema);
--- app/routes/income.js
+++ app/routes/income.js
@@ -1,95 +1,96 @@
-var moment = require('moment');
-var Income = require('../models/income');
+var moment = require("moment");
+var Income = require("../models/income");
 
-var exports = module.exports = {};
+var exports = (module.exports = {});
 
 function validate_request(req, res) {
-  body = req.body
+  body = req.body;
 
-  if(!body.date || body.date.length == 0)
-    return {message: "Date not informed"}
-  if(!body.quantity || body.quantity.length == 0)
-    return {message: "Quantity not informed"}
-  if(!body.value || body.value.length == 0)
-    return {message: "Value not informed"}
+  if (!body.date || body.date.length == 0)
+    return { message: "Date not informed" };
+  if (!body.quantity || body.quantity.length == 0)
+    return { message: "Quantity not informed" };
+  if (!body.value || body.value.length == 0)
+    return { message: "Value not informed" };
 }
 
 function parse_request(income, investment, body) {
-  income.investment = investment
+  income.investment = investment;
   income.date = moment(body.date, "MM/YYYY");
   income.quantity = body.quantity;
   income.value = body.value;
   income.bought = body.bought;
+  income.gross = body.gross;
+  income.ir = body.ir;
+  income.tax = body.tax;
 }
 
 function root(router) {
-  router.route('/investments/:investment_id/income')
+  router
+    .route("/investments/:investment_id/income")
     .get(function(req, res) {
-        Income.find({investment: req.params.investment_id}).sort('-date').exec(function(err, incomes) {
-          if (err)
-            res.send(err);
+      Income.find({ investment: req.params.investment_id })
+        .sort("-date")
+        .exec(function(err, incomes) {
+          if (err) res.send(err);
 
           res.json(incomes);
         });
-      })
+    })
     .post(function(req, res) {
-        var income = new Income();
+      var income = new Income();
+      validation = validate_request(req, res);
+      if (validation) {
+        res.status(400).send(validation);
+        return;
+      }
+      parse_request(income, req.params.investment_id, req.body);
+
+      income.save(function(err, income) {
+        if (err) res.send(err);
+
+        res.json(income);
+      });
+    });
+}
+
+function model(router) {
+  router
+    .route("/investments/:investment_id/income/:income_id")
+    .get(function(req, res) {
+      Income.findById(req.params.income_id, function(err, income) {
+        if (err) res.send(err);
+        res.json(income);
+      });
+    })
+    .put(function(req, res) {
+      Income.findById(req.params.income_id, function(err, income) {
+        if (err) res.send(err);
+
         validation = validate_request(req, res);
-        if(validation) {
+        if (validation) {
           res.status(400).send(validation);
           return;
         }
         parse_request(income, req.params.investment_id, req.body);
 
         income.save(function(err, income) {
-          if (err)
-            res.send(err);
+          if (err) res.send(err);
 
           res.json(income);
         });
       });
-}
-
-function model(router) {
-  router.route('/investments/:investment_id/income/:income_id')
-    .get(function(req, res) {
-        Income.findById(req.params.income_id, function(err, income) {
-          if (err)
-            res.send(err);
-          res.json(income);
-        });
-      })
-    .put(function(req, res) {
-        Income.findById(req.params.income_id, function(err, income) {
-          if (err)
-            res.send(err);
-
-          validation = validate_request(req, res);
-          if(validation) {
-            res.status(400).send(validation);
-            return;
-          }
-          parse_request(income, req.params.investment_id, req.body);
-
-          income.save(function(err, income) {
-            if (err)
-              res.send(err);
-
-            res.json(income);
-          });
-        });
-      })
+    })
     .delete(function(req, res) {
-        Income.remove({_id: req.params.income_id}, function(err, income) {
-          if (err)
-            res.send(err);
+      Income.remove({ _id: req.params.income_id }, function(err, income) {
+        if (err) res.send(err);
 
-          res.json(income);
-        });
+        res.json(income);
+      });
     });
 }
 
 exports.map_routes = function(router) {
   root(router);
   model(router);
-}
+};
