cad3aba0906c09e19b0e2c99539bc4dd5f01d699
Jonathan Zanella <jonathanrafaelz@gmail.com>
2017-11-15 23:53:22 -0200

    Change to use mongoose exec

--- app/routes/investment.js
+++ app/routes/investment.js
@@ -24,42 +24,44 @@ function root(router) {
   router
     .route("/investments")
     .get((req, res) => {
-      Investment.find((err, investments) => {
-        if (err) res.send(err);
+      Investment.find()
+        .exec((err, investments) => {
+          if (err) res.send(err);
 
-        const queriesPromises = [];
+          const queriesPromises = [];
 
-        console.log(`with_incomes=${req.query.with_incomes}`);
-        console.log(`query=${req.query}`);
-        console.log(`params=${req.params}`);
-        if (req.query.with_incomes) {
-          console.log(`inside if`);
-          investments.forEach(investment => {
-            console.log(`investment=${investment.name}`);
-            const queryPromise = Income.find({
-              investment: investment._id
-            }).exec();
-            queriesPromises.push(queryPromise);
-            queryPromise.then(incomes => {
-              investment.incomes = incomes;
-              console.log(
-                `investment=${investment.name} incomes=${investment.incomes
-                  .length}`
-              );
+          console.log(`with_incomes=${req.query.with_incomes}`);
+          console.log(`query=${req.query}`);
+          console.log(`params=${req.params}`);
+          if (req.query.with_incomes) {
+            console.log(`inside if`);
+            investments.forEach(investment => {
+              console.log(`investment=${investment.name}`);
+              const queryPromise = Income.find({
+                investment: investment._id
+              }).exec();
+              queriesPromises.push(queryPromise);
+              queryPromise.then(incomes => {
+                investment.incomes = incomes;
+                console.log(
+                  `investment=${investment.name} incomes=${investment.incomes
+                    .length}`
+                );
+              });
             });
-          });
-          console.log(`end of if`);
-        }
+            console.log(`end of if`);
+          }
 
-        console.log(`promises=${queriesPromises.length}`);
-        Promise.all(queriesPromises).then(() => {
-          console.log(`incomes=${investments[0].incomes.length}`);
-          res.json(investments);
+          console.log(`promises=${queriesPromises.length}`);
+          Promise.all(queriesPromises).then(() => {
+            console.log(`incomes=${investments[0].incomes.length}`);
+            res.json(investments);
+          });
+          return false;
+        })
+        .catch(err => {
+          res.send(err);
         });
-        return false;
-      }).catch(err => {
-        res.send(err);
-      });
     })
     .post((req, res) => {
       const investment = new Investment();
